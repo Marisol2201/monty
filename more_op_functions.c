77ce40046d1422d73b232b7447e08e8204791eaa
monicajoa <mnortiz.ortiz@gmail.com>
2020-05-15 05:19:38 +0000

    change funtion

--- more_op_functions.c
+++ more_op_functions.c
@@ -1,4 +1,5 @@
 #include "monty.h"
+
 /**
  * op_add - add two elements on the top of the stack
  * @stack: pointer to the head of the stack
@@ -51,6 +52,7 @@ size_t dlistint_len(stack_t *h)
 	}
 	return (n_nodes);
 }
+
 /**
  * op_nop - do nothing
  * @stack: pointer to the head of the stack
@@ -63,6 +65,7 @@ void op_nop(stack_t **stack, unsigned int line_number)
 	stack = stack;
 	line_number = line_number;
 }
+
 /**
  * op_pop - delete the element on the top of the stack
  * @stack: pointer to the head of the stack
@@ -89,4 +92,35 @@ void op_pop(stack_t **stack, unsigned int line_number)
 		(*stack)->prev = NULL;
 	}
 	free(temp);
+}
+
+/**
+ * op_sub - sub two elements on the top of the stack
+ * @stack: pointer to the head of the stack
+ * @line_number: number of the current line
+ *
+ * Return: the number of nodes
+ */
+void op_sub(stack_t **stack, unsigned int line_number)
+{
+	size_t n = 0;
+	stack_t *temp = *stack;
+
+	line_number = line_number;
+	n = dlistint_len(slayer.stack_head);
+	if (n < 2)
+	{
+		slayer_list(slayer.stack_head);
+		free(slayer.getl_info);
+		fclose(slayer.fp_struct);
+		fprintf(stderr, "L%u: can't sub, stack too short\n", slayer.n_lines);
+		exit(EXIT_FAILURE);
+	}
+	if (*stack != NULL)
+	{
+		*stack = (*stack)->next;
+		(*stack)->n = (*stack)->n - (*stack)->prev->n;
+		(*stack)->prev = NULL;
+		free(temp);
+	}
 }
