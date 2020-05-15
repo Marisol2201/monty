4995600f9f681eda622862464e456c1654e392ad
monicajoa <mnortiz.ortiz@gmail.com>
2020-05-15 01:52:03 +0000

    new header version 1

--- opcode.c
+++ opcode.c
@@ -1,43 +1,33 @@
 #include "monty.h"
 
 /**
- *  opcode - choose the function to execute
- *  @file_line: pointer to the information in each line of the file
- *  @stack: pointer to stack structure
- *  @n_lines: current line count of the file
- *  Return: int
+ * opcode - interpreter operations
+ * @command: content line
+ *
+ * Return: void
  */
-
-int opcode(char *file_line, stack_t **stack, unsigned int n_lines)
+void opcode(char *command)
 {
-	char **key = NULL;
-	unsigned int i;
-	/* printf("El conteo va en: %d\n", n_lines); */
+	unsigned int i = 0;
 	instruction_t opcode_func[] = {
-	    {"push", op_push},
-	    {"pall", op_pall},
-	    {NULL, NULL}};
-	key = str_to_2d(file_line, " \n");
-	/* printf("El key actual es: %s...\n", key[0]); */
-	if (strcmp(key[0], "push") == 0)
-	{
-		is_digit(key[1], n_lines, key);
-		node_data = atoi(key[1]);
-	}
+		{"push", op_push},
+		{"pall", op_pall},
+		{NULL, NULL},
+	};
+
 	i = 0;
-	while (opcode_func[i].opcode != NULL)
+	while ((opcode_func[i].opcode != NULL))
 	{
-		/* printf("%s\n", opcode_func[i].opcode); */
-		if (strcmp(opcode_func[i].opcode, key[0]) == 0)
+		if (strcmp(opcode_func[i].opcode, command) == 0)
 		{
-			/* printf("im inside if\n"); */
-			opcode_func[i].f(stack, n_lines);
-			free_grid(key);
-			return (1);
+			opcode_func[i].f(&slayer.stack_head, slayer.n_lines);
+			return;
 		}
 		i++;
 	}
-	fprintf(stderr, "L'%u': unknown instruction '%s'\n", n_lines, key[0]);
+	fprintf(stderr, "L%u: unknown instruction %s\n", slayer.n_lines, command);
+	free(slayer.getl_info);
+	slayer_list(slayer.stack_head);
+	fclose(slayer.fp_struct);
 	exit(EXIT_FAILURE);
-	return (1);
 }
