544ced8cc6134ddb444725026debe537e00e8a62
monicajoa <mnortiz.ortiz@gmail.com>
2020-05-15 04:26:18 +0000

    fixed3 opcode function

--- opcode.c
+++ opcode.c
@@ -1,4 +1,4 @@
- #include "monty.h"
+#include "monty.h"
 
 /**
  * opcode - interpreter operations
@@ -10,14 +10,14 @@ void opcode(char *command)
 {
 	unsigned int i = 0;
 	instruction_t opcode_func[] = {
-	    {"push", op_push},
-	    {"pall", op_pall},
-	    {"pint", op_pint},
-	    /*{"swap", op_swap},*/
-	    /*{"pop", op_pop},*/
-	    /*{"add", op_add},*/
-	    /*{"nop", op_nop},*/
-	    {NULL, NULL},
+		{"push", op_push},
+		{"pall", op_pall},
+		{"pint", op_pint},
+		{"swap", op_swap},
+		{"pop", op_pop},
+		{"add", op_add},
+		{"nop", op_nop},
+		{NULL, NULL},
 	};
 
 	while ((opcode_func[i].opcode != NULL))
